8a2f4316b67700a5bed4e676bd8a1b80f9df0046
Sam <wankhadesaumyak@gmail.com>
2026-07-18 21:35:59 +0530

    Fix project structure by replacing files with folders

---
+++ src/main.py
@@ -0,0 +1,9 @@
+def main():
+    print("=" * 50)
+    print("Welcome to BuddhaTrader")
+    print("Version 0.1")
+    print("From 'Day One' to 'One Day'")
+    print("=" * 50)
+
+if __name__ == "__main__":
+    main()
